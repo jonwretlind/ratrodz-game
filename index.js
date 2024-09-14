1a2259be7ceba36a13daa86c6fa70a60551046a6
jonwretlind <jcwretlind@aol.com>
2024-09-14 13:17:44 -0600

    updated for Heroku deploy

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ const PORT = 5000;
 app.use('/', express.static('./src', { index: "index.html" }));
 
 app.listen(
-  PORT,
+  process.env.PORT || PORT,
   () => console.log('Server running on port %d ...', PORT)
 );
